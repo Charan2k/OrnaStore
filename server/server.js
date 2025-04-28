eb5589eadf14c7c23183e80310c3e6bbdc2c61c2
aksvod37 <akshay-murari.vodela@siemens.com>
2025-04-28 15:29:35 +0530

    collect ssl options if runs on secured port

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const {metalPricesCronJob} = require("./cron/metalPricesCronJob.js");
+const { metalPricesCronJob } = require("./cron/metalPricesCronJob.js");
 const sequelize = require("./config/database.js");
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
+
+// Read SSL certificates dynamically from environment variables
+let sslOptions = {};
+if (process.env.SSL_KEY_PATH && process.env.SSL_CERT_PATH) {
+    sslOptions = {
+        key: fs.readFileSync(process.env.SSL_KEY_PATH),
+        cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+
+    // Include CA bundle if it's provided
+    if (process.env.SSL_CA_PATH) {
+        sslOptions.ca = fs.readFileSync(process.env.SSL_CA_PATH);
+    }
+}
 
 app.use(cors());
 app.use(express.json());
@@ -36,14 +50,21 @@ const startServer = async () => {
 
         // Start the cron job(s)
         if (process.env.CRON_JOB_ENABLED === "true") {
-			metalPricesCronJob();
+            metalPricesCronJob();
         }
 
         // Start the Express server
         const PORT = process.env.PORT || 8000;
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
+        if (Object.keys(sslOptions).length > 0) {
+            const https = require("https");
+            https.createServer(sslOptions, app).listen(443, () => {
+                console.log(`Server running on HTTPS at port 443`);
+            });
+        } else {
+            app.listen(PORT, () => {
+                console.log(`Server running on HTTP at port ${PORT}`);
+            });
+        }
     } catch (error) {
         console.error("Error starting the server:", error);
         process.exit(1); // Exit if there's an error during setup
