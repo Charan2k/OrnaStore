7406e407a38fa8c77af5c4c62aca4af0bb0ef2a6
aksvod37 <akshay-murari.vodela@siemens.com>
2025-04-30 14:49:55 +0530

    shift to hash router

--- web/src/utils/adminLogin.js
+++ web/src/utils/adminLogin.js
@@ -1,7 +1,7 @@
 const adminLogin = (response) => {
     console.log(response)
     localStorage.setItem("adminToken", response.data.token);
-    window.location.href = "/admin/dashboard";
+    window.location.href = "/#/admin/dashboard";
 }
 
 export default adminLogin;
